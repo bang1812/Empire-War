2f6706a642fe8e68dc320a9a13b9ecf2f2ff3f61
bang1812 <luongbangtsk03032@gmail.com>
2024-12-06 02:45:20 +0700

    the first commit

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float speed;
+}
